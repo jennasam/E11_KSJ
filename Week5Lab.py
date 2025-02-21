fcfeeb622479e66fdaabf40cb9bb386302fa3dac
sahejroopsethi <sahejroop.sethi@gmail.com>
2025-02-21 15:01:46 -0800

    Update Week5Lab.py

--- Week5Lab.py
+++ Week5Lab.py
@@ -45,27 +45,27 @@ def combodata(time1):
         i += 1
 
 
-            print()
-            print("Concentration Units (standard)")
-            print("---------------------------------------")
-            print(
-                "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-                % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
-            )
-            print("Concentration Units (environmental)")
-            print("---------------------------------------")
-            print(
-                "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-                % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
-            )
-            print("---------------------------------------")
-            print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
-            print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
-            print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
-            print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
-            print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
-            print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
-            print("---------------------------------------")
+        print()
+        print("Concentration Units (standard)")
+        print("---------------------------------------")
+        print(
+            "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+            % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+        )
+        print("Concentration Units (environmental)")
+        print("---------------------------------------")
+        print(
+            "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+            % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+        )
+        print("---------------------------------------")
+        print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+        print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+        print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+        print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+        print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+        print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+        print("---------------------------------------")
 
         file.close()
 
