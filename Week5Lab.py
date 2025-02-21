6eab0571c445fabbc15e19f17b02b7746703b911
sahejroopsethi <sahejroop.sethi@gmail.com>
2025-02-21 15:00:48 -0800

    updated

--- Week5Lab.py
+++ Week5Lab.py
@@ -24,49 +24,49 @@ import time
 from datetime import datetime
 file = open("PM_Ada.csv", "w", newline = None)
 file_writer = csv.writer(file)
-file_writer.writerow(("Time", "PM1.0", "PM2.5", "PM10"))
-time1 = 0
+file_writer.writerow(("Time", "PM1.0", "PM2.5", "PM10", "Temp", "Gas", "Humidity", "Pressure", "Altitude"))
 
 i2c = board.I2C()
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 bme680.sea_level_pressure = 1013.25
 
+def combodata(time1):
+    while i <= time1:
+        time.sleep(1)
+        #print("Seconds: ", time1, "Temperature: ",bme680.temperature,"Gas: ", bme680.gas, "Humidity:",bme680.relative_humidity, "Pressure: ", bme680.pressure, "Altitude: ", bme680.altitude)
 
-while time1 <= 30:
-    time.sleep(1)
-    print("Seconds: ", time1, "Temperature: ",bme680.temperature,"Gas: ", bme680.gas, "Humidity:",bme680.relative_humidity, "Pressure: ", bme680.pressure, "Altitude: ", bme680.altitude)
+        try:
+            aqdata = pm25.read()
+        except RuntimeError:
+            print("Unable to read from sensor, retrying...")
+            continue
 
-    try:
-        aqdata = pm25.read()
-        # print(aqdata)
-    except RuntimeError:
-        print("Unable to read from sensor, retrying...")
-        continue
-
-    file_writer.writerow([datetime.fromtimestamp(time.time()), aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"]])
-    time1 += 1
+        file_writer.writerow([datetime.fromtimestamp(time.time()), aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"], bme680.temperature, bme680.gas, bme680.relative_humidity, bme680.pressure, bme680.altitude])
+        i += 1
 
 
-    print()
-    print("Concentration Units (standard)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
-    )
-    print("Concentration Units (environmental)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
-    )
-    print("---------------------------------------")
-    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
-    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
-    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
-    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
-    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
-    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
-    print("---------------------------------------")
+            print()
+            print("Concentration Units (standard)")
+            print("---------------------------------------")
+            print(
+                "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+                % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+            )
+            print("Concentration Units (environmental)")
+            print("---------------------------------------")
+            print(
+                "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+                % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+            )
+            print("---------------------------------------")
+            print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+            print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+            print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+            print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+            print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+            print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+            print("---------------------------------------")
 
-file.close()
+        file.close()
+
+    return
