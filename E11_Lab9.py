bfcd9395c53e24b76dc81bbb00b00d7c7349b53c
sahejroopsethi <sahejroop.sethi@gmail.com>
2025-03-21 15:23:28 -0700

    Update E11_Lab9.py

--- E11_Lab9.py
+++ E11_Lab9.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 import datetime
+import time
  
 def my_callback(channel):
     GPIO.input(channel) == GPIO.HIGH
     print('I got an output!')
  
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN)
-    GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(6, GPIO.IN)
+GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
   
-finally:
-    GPIO.cleanup()
+while true:
+    time.sleep(10)
+
+    
