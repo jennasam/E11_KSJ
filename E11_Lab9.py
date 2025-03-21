22687cfe56eea2906ca376981267e730f5745da0
sahejroopsethi <sahejroop.sethi@gmail.com>
2025-03-21 15:12:10 -0700

    Create E11_Lab9.py

---
+++ E11_Lab9.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import datetime
+ 
+def my_callback(channel):
+    GPIO.input(channel) == GPIO.HIGH:
+    print('I got an output!')
+ 
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(6, GPIO.IN)
+    GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
+ 
+    message = raw_input('\nPress any key to exit.\n')
+ 
+finally:
+    GPIO.cleanup()
