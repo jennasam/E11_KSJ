9b29d4568d80137a9c9410ef82ed651544412e17
sahejroopsethi <sahejroop.sethi@gmail.com>
2025-03-21 15:56:16 -0700

    Update E11_Lab9.py

--- E11_Lab9.py
+++ E11_Lab9.py
@@ -11,22 +11,21 @@ x = 0
 cpm = 0
 
 def my_callback(channel):
-    time.sleep(1)
     GPIO.input(channel) == GPIO.HIGH
     global count
-    global x
     global cpm
     count += 1
     now = datetime.datetime.now()
-    x += 1
-    if (x % 60) == 0:
-        cpm = count
-    print(f'I got an output! The count number is {count}. The time is {now}. CPM is {cpm}.') 
+    print(f'I got an output! The count number is {count}. The time is {now}.') 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(6, GPIO.IN)
 GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
   
 while True:
-    time.sleep(10)
-
+    time.sleep(1)
+    global x
+    x += 1
+    if (x % 60) == 0:
+        cpm = count
+        print(f'CPM is {cpm}')
