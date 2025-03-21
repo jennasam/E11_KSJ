23476a725516208f7643521916364a352522a8f0
sahejroopsethi <sahejroop.sethi@gmail.com>
2025-03-21 15:44:30 -0700

    Update E11_Lab9.py

--- E11_Lab9.py
+++ E11_Lab9.py
@@ -6,6 +6,7 @@ import datetime
 global count
 count = 0
 x = 0
+cpm = 0
 
 def my_callback(channel):
     time.sleep(1)
@@ -16,7 +17,7 @@ def my_callback(channel):
     x += 1
     if (x % 60) == 0:
         cpm = count
-    print(f'I got an output! The count number is {count}. The time is {now}. CPM is {cpm}') 
+    print(f'I got an output! The count number is {count}. The time is {now}. CPM is {cpm}.') 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(6, GPIO.IN)
